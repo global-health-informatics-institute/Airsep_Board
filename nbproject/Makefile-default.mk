e423aa770075cee564d3b71a11e2e7280bda36eb
FrancisArmader <francissambanii@gmail.com>
2021-12-07 08:02:31 +0200

    initial commit

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -39,7 +39,7 @@ FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/Airsep_Control_Codee.X.${IMAGE_TYPE}.
 endif
 
 ifeq ($(COMPARE_BUILD), true)
-COMPARISON_BUILD=-mafrlcsj
+COMPARISON_BUILD=
 else
 COMPARISON_BUILD=
 endif
